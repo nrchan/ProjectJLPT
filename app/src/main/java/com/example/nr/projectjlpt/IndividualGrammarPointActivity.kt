765b247ce07a9b02d1ab45898ddec1891f14053a
nrchan <luka.iwt@gmail.com>
2019-07-03 16:52:32 +0800

    create activity for individual grammar point

--- app/src/main/java/com/example/nr/projectjlpt/IndividualGrammarPointActivity.kt
+++ app/src/main/java/com/example/nr/projectjlpt/IndividualGrammarPointActivity.kt
@@ -2,11 +2,26 @@ package com.example.nr.projectjlpt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_individual_grammar_point.*
 
 class IndividualGrammarPointActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_individual_grammar_point)
+
+        val grammar = GrammarPoint(
+            null,
+            intent.getStringExtra("pattern"),
+            intent.getStringExtra("kana"),
+            intent.getStringExtra("romaji"),
+            intent.getStringExtra("meaning"),
+            intent.getIntExtra("level", 0),
+            intent.getStringExtra("explanation"),
+            intent.getStringExtra("usage")
+        )
+
+        textView.text = grammar.pattern
+
     }
 }
