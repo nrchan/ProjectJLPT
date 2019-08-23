def85f9cc0b4fa5b61ee78dc46cd04e8b89e7de3
nrchan <luka.iwt@gmail.com>
2019-08-23 18:04:31 +0800

    conjugator now omit the u after verb

--- app/src/main/java/com/example/nr/projectjlpt/ConjugatorActivity.kt
+++ app/src/main/java/com/example/nr/projectjlpt/ConjugatorActivity.kt
@@ -8,8 +8,11 @@ import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import kotlinx.android.synthetic.main.activity_conjugator.*
+import kotlinx.android.synthetic.main.activity_individual_grammar_point.*
 import kotlinx.android.synthetic.main.activity_setting.*
 
 class ConjugatorActivity : AppCompatActivity() {
@@ -38,17 +41,75 @@ class ConjugatorActivity : AppCompatActivity() {
             val query = verb_input_editText.text.toString()
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+            verb_input_editText.clearFocus()
 
             if(query.isNullOrBlank()) return@setOnEditorActionListener false
             else search(query)
             true
         }
+
+        verb_input_editText.setOnFocusChangeListener { view, b ->
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 
     private fun search(query : String) {
         val db = FirebaseFirestore.getInstance()
-        val docRef = db.collection("verb").document(query)
-        docRef.get()
-        Toast.makeText(applicationContext, query, Toast.LENGTH_SHORT).show()
+        val docRef = db.collection("verb")
+        var myQuery : Query
+        when {
+            query.matches(Regex("^[ぁ-んァ-ヴｦ-ﾟ]+$")) ->
+                myQuery = docRef.whereEqualTo("stem-kana", queriable(query, 0))
+            query.matches(Regex("^[A-Za-z]+$")) ->
+                myQuery = docRef.whereEqualTo("stem-romaji", queriable(query, 1))
+            else ->
+                myQuery = docRef.whereEqualTo("stem-kanji", queriable(query, 0))
+        }
+
+        myQuery.get()
+            .addOnSuccessListener {
+                if(it.isEmpty) {
+                    Toast.makeText(applicationContext, "Sorry, I can't find the conjugation for $query", Toast.LENGTH_SHORT).show()
+                    return@addOnSuccessListener
+                }
+                it.forEach {
+                    Toast.makeText(
+                        applicationContext,
+                        it.getString("stem-kana"),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(applicationContext, "Sorry, something went wrong...", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    //turn the raw query into the form (stem-only form) stored in database
+    private fun queriable(query : String, mode : Int) : String{
+        var final = query
+        when (mode) {
+            0 -> if(query.endsWith("う") || query.endsWith("つ") || query.endsWith("る") ||
+                query.endsWith("く") || query.endsWith("ぐ") || query.endsWith("ぬ") ||
+                query.endsWith("ぶ") || query.endsWith("む") || query.endsWith("す")) {
+                final = query.dropLast(1)
+            }
+            1 -> when {
+                query.endsWith("tsu", true) -> {
+                    final = query.dropLast(3)
+                }
+                query.endsWith("ru", true) || query.endsWith("ku", true) ||
+                        query.endsWith("gu", true) || query.endsWith("nu", true) ||
+                        query.endsWith("gu", true) || query.endsWith("mu", true) ||
+                        query.endsWith("su", true) -> {
+                    final = query.dropLast(2)
+                }
+                query.endsWith("u", true) -> {
+                    final = query.dropLast(1)
+                }
+            }
+        }
+        return final
     }
 }
