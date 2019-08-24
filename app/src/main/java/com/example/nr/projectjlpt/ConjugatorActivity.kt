a050aaca662223b5ade0e9446a6a0181d6d3373a
nrchan <luka.iwt@gmail.com>
2019-08-24 20:57:26 +0800

    finish conjugate in most form

--- app/src/main/java/com/example/nr/projectjlpt/ConjugatorActivity.kt
+++ app/src/main/java/com/example/nr/projectjlpt/ConjugatorActivity.kt
@@ -111,10 +111,94 @@ class ConjugatorActivity : AppCompatActivity() {
                 var stemJa : String? = verb.stemKanji
                 if (stemJa.isNullOrBlank()) stemJa = verb.stemKana
                 var stemRomaji : String? = verb.stemRomaji
-                dict_stemJa.text =  stemJa
-                dict_conjugation.text = verb.getDict()
-                dict_stemRomaji.text = stemRomaji
-                dict_conjugationRomaji.text = verb.getDictRomaji()
+
+                myVerb.text = getString(R.string.conjugator_activity_myVerb, stemJa, verb.dict())
+                if(verb.group == "くる") myVerb.text = verb.dict()
+                myRomaji.text = getString(R.string.conjugator_activity_myRomaji, verb.stemRomaji, verb.dictRomaji())
+                if(verb.group == "くる") myRomaji.text = verb.dictRomaji()
+                myMeaning.text = getString(R.string.conjugator_activity_myMeaning, verb.meaning)
+                myGroup.text = when(verb.group) {
+                    "う" -> getString(R.string.conjugator_activity_myGroup_group1, "u")
+                    "つ" -> getString(R.string.conjugator_activity_myGroup_group1, "tsu")
+                    "る" -> getString(R.string.conjugator_activity_myGroup_group1, "ru")
+                    "く" -> getString(R.string.conjugator_activity_myGroup_group1, "ku")
+                    "ぐ" -> getString(R.string.conjugator_activity_myGroup_group1, "gu")
+                    "ぬ" -> getString(R.string.conjugator_activity_myGroup_group1, "nu")
+                    "ぶ" -> getString(R.string.conjugator_activity_myGroup_group1, "bu")
+                    "む" -> getString(R.string.conjugator_activity_myGroup_group1, "mu")
+                    "す" -> getString(R.string.conjugator_activity_myGroup_group1, "su")
+                    "いる" -> getString(R.string.conjugator_activity_myGroup_group2, "iru")
+                    "える" -> getString(R.string.conjugator_activity_myGroup_group2, "eru")
+                    "する" -> getString(R.string.conjugator_activity_myGroup_group3, "suru")
+                    "くる" -> getString(R.string.conjugator_activity_myGroup_group3, "kuru")
+                    else -> ""
+                }
+
+                if(verb.group != "くる") dict_stemJa.text =  stemJa
+                dict_conjugation.text = verb.dict()
+                if(verb.group != "くる") dict_stemRomaji.text = stemRomaji
+                dict_conjugationRomaji.text = verb.dictRomaji()
+
+                if(verb.group != "くる") neg_stemJa.text =  stemJa
+                neg_conjugation.text = verb.neg()
+                if(verb.group != "くる") neg_stemRomaji.text = stemRomaji
+                neg_conjugationRomaji.text = verb.negRomaji()
+
+                if(verb.group != "くる") masu_stemJa.text =  stemJa
+                masu_conjugation.text = verb.masu()
+                if(verb.group != "くる") masu_stemRomaji.text = stemRomaji
+                masu_conjugationRomaji.text = verb.masuRomaji()
+
+                if(verb.group != "くる") te_stemJa.text =  stemJa
+                te_conjugation.text = verb.te()
+                if(verb.group != "くる") te_stemRomaji.text = stemRomaji
+                te_conjugationRomaji.text = verb.teRomaji()
+
+                if(verb.group != "くる") ta_stemJa.text =  stemJa
+                ta_conjugation.text = verb.ta()
+                if(verb.group != "くる") ta_stemRomaji.text = stemRomaji
+                ta_conjugationRomaji.text = verb.taRomaji()
+
+                if(verb.group != "くる") pot_stemJa.text =  stemJa
+                pot_conjugation.text = verb.pot()
+                if(verb.group != "くる") pot_stemRomaji.text = stemRomaji
+                pot_conjugationRomaji.text = verb.potRomaji()
+
+                if(verb.group != "くる") con_stemJa.text =  stemJa
+                con_conjugation.text = verb.con()
+                if(verb.group != "くる") con_stemRomaji.text = stemRomaji
+                con_conjugationRomaji.text = verb.conRomaji()
+
+                if(verb.group != "くる") vol_stemJa.text =  stemJa
+                vol_conjugation.text = verb.vol()
+                if(verb.group != "くる") vol_stemRomaji.text = stemRomaji
+                vol_conjugationRomaji.text = verb.volRomaji()
+
+                if(verb.group != "くる") proh_stemJa.text =  stemJa
+                proh_conjugation.text = verb.proh()
+                if(verb.group != "くる") proh_stemRomaji.text = stemRomaji
+                proh_conjugationRomaji.text = verb.prohRomaji()
+
+                if(verb.group != "くる") imp_stemJa.text =  stemJa
+                imp_conjugation.text = verb.imp()
+                if(verb.group != "くる") imp_stemRomaji.text = stemRomaji
+                imp_conjugationRomaji.text = verb.impRomaji()
+
+                if(verb.group != "くる") cau_stemJa.text =  stemJa
+                cau_conjugation.text = verb.cau()
+                if(verb.group != "くる") cau_stemRomaji.text = stemRomaji
+                cau_conjugationRomaji.text = verb.cauRomaji()
+
+                if(verb.group != "くる") pas_stemJa.text =  stemJa
+                pas_conjugation.text = verb.pas()
+                if(verb.group != "くる") pas_stemRomaji.text = stemRomaji
+                pas_conjugationRomaji.text = verb.pasRomaji()
+
+                if(verb.group != "くる") caupas_stemJa.text =  stemJa
+                caupas_conjugation.text = verb.caupas()
+                if(verb.group != "くる") caupas_stemRomaji.text = stemRomaji
+                caupas_conjugationRomaji.text = verb.caupasRomaji()
+
                 conjugation_result_view.apply {
                     alpha = 0f
                     visibility = View.VISIBLE
